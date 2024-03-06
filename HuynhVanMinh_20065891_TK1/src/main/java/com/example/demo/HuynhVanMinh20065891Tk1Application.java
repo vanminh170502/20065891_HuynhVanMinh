59e8f33a65cb8975cc78c9059c8aa01eee892cb5
H31M44\Student <Student>
2024-03-06 13:02:36 +0700

    upload project

---
+++ HuynhVanMinh_20065891_TK1/src/main/java/com/example/demo/HuynhVanMinh20065891Tk1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhVanMinh20065891Tk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuynhVanMinh20065891Tk1Application.class, args);
+	}
+
+}
---
+++ HuynhVanMinh_20065891_TK1/src/test/java/com/example/demo/HuynhVanMinh20065891Tk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuynhVanMinh20065891Tk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
